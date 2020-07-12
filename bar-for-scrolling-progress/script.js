3e8d04f3fc9705c0af85387fae6c4e86530188f6
thomaz-s <63955736+thomaz-s@users.noreply.github.com>
2020-07-12 16:12:16 -0300

    Add files via upload

---
+++ bar-for-scrolling-progress/script.js
@@ -0,0 +1,33 @@
+function fillPs(section, lenght){
+    const p = document.querySelector(`${section} p`);
+    for(var i=0; i<lenght; i++){
+        p.textContent += `${section} `;
+    }
+}
+
+fillPs('header', 300);
+fillPs('main', 6000);
+fillPs('footer', 300);
+
+const bar = document.createElement('div');
+document.body.append(bar);
+
+bar.style.height = '4px';
+bar.style.width = '0px';
+bar.style.backgroundColor = '#0f0';
+bar.style.position = 'fixed';
+bar.style.top = '0';
+bar.style.left = '0';
+bar.style.zIndex = '10';
+bar.style.transition = '0.2s';
+
+const p = document.querySelector('main p');
+
+window.addEventListener('load', ()=>{
+    document.addEventListener('scroll', ()=>{
+        const textHeight = p.offsetHeight;
+        const pagePosition = window.pageYOffset;
+        const percentage = pagePosition * 100 / textHeight;
+        bar.style.width = Math.round(percentage) + '%';
+    });
+});
